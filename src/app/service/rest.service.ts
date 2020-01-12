d35e6d37254b17cdeea9de69a8e6fbe28d0c77e5
afshar <afshartech@gmail.com>
2020-01-12 20:39:56 +0530

    Added support to continuosly send stream of data to server

---
+++ src/app/service/rest.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class RestService {
+    constructor(private httpClient: HttpClient) {
+    }
+
+    postData(item: StreamItem): void {
+      
+
+      const serverEndpoint = `http://localhost:5000/nucleus/v1/consume`;
+      //return this.httpClient.post(`${this.api}/${serverEndpoint}`) as Observable<EdisonAIAlgorithm[]>;
+      this.httpClient.post(`${serverEndpoint}`, item).subscribe();
+
+      
+    }
+  }
+
+export interface StreamItem {
+  deviceId: string,
+  deviceType: string,
+  timestamp: string,
+  value: string
+}
+
